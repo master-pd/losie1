78e8130a882b1462b96f5a1dd1e5272c029685e0
MAR PD <mdranasheikhe2005@gmail.com>
2026-02-03 02:02:10 +0600

    Create reply.js

---
+++ gen/reply.js
@@ -0,0 +1,14 @@
+module.exports = {
+  messages: [
+    "Interesting! 🤔",
+    "Thanks for sharing! 😊",
+    "That's cool! 😎",
+    "I see! 👀",
+    "Tell me more! 💬",
+    "Nice one! 👍",
+    "Haha! 😂",
+    "Wow! 😮",
+    "Amazing! ✨",
+    "Keep it up! 🚀"
+  ]
+};
